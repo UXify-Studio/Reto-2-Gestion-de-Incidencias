68f26a4c8b003d3fea611ce3971700b337e146b9
Adrian Lopez <adriiloma2005@gmail.com>
2025-01-09 12:01:26 +0100

    laravel y vue conectado con templates de prueba en vue

---
+++ Codigo/uxify-frontend/src/components/UserList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Users</h1>
+        <ul>
+            <li v-for="user in users" :key="user.id">{{ user.name }} - {{ user.email }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            users: []
+        };
+    },
+    created() {
+        axios.get('http://127.0.0.1:8000/api/users')
+            .then(response => {
+                this.users = response.data;
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+};
+</script>
