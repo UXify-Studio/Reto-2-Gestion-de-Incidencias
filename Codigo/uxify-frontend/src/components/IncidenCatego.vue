8cbc8e8a913a01a516cc91f39e89ffe6c4de9620
ZahirRiveraChacon <zahirallonso.rivera@ikasle.egibide.org>
2025-01-16 09:36:08 +0100

    tablas y mas info

---
+++ Codigo/uxify-frontend/src/components/IncidenCatego.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container mt-4">
+      <h2 class="text-primary mb-1 fs-4">Incidencias por Categoría</h2>
+      <table class="table table-bordered table-striped">
+        <thead class="table-light"> 
+          <tr>
+            <th class="p-1 text-center align-middle">Categoría</th>
+            <th class="p-1 text-center align-middle">Prioridad Baja</th>
+            <th class="p-1 text-center align-middle">Prioridad Media</th>
+            <th class="p-1 text-center align-middle">Prioridad Alta</th>
+            <th class="p-1 text-center align-middle">Incumplidos</th>
+            <th class="p-1 text-center align-middle">En proceso</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="categoria in categorias" :key="categoria.nombre">
+            <td class="text-center align-middle">{{ categoria.nombre }}</td>
+            <td class="text-center align-middle">{{ categoria.baja }}</td>
+            <td class="text-center align-middle">{{ categoria.media }}</td>
+            <td class="text-center align-middle">{{ categoria.alta }}</td>
+            <td class="text-center align-middle">{{ categoria.incumplidos }}</td>
+            <td class="text-center align-middle">{{ categoria.enProceso }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'IncidenciasPorCategoria',
+    data() {
+      return {
+        categorias: [
+          { nombre: 'Eléctrico', baja: 2, media: 0, alta: 0, incumplidos: 0, enProceso: 0 },
+          { nombre: 'Mecánico', baja: 22, media: 1, alta: 0, incumplidos: 16, enProceso: 5 },
+          { nombre: 'Categoría 3', baja: 1, media: 0, alta: 0, incumplidos: 2, enProceso: 0 },
+          { nombre: 'Categoría 4', baja: 0, media: 0, alta: 0, incumplidos: 0, enProceso: 0 },
+        ],
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  
+  </style>
---
+++ Codigo/uxify-frontend/src/components/SituacionTick.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h3>Situación tickets</h3>
+      <hr>
+      <p><span class="font-weight-bold">{{ tickets.activas }}</span> Incidencias Activas</p>
+      <p><span class="font-weight-bold">{{ tickets.pendientes }}</span> Incidencias Pendientes</p>
+      <p><span class="font-weight-bold">{{ tickets.sinAsignar }}</span> Incidencias sin asignar</p>
+      <p><span class="font-weight-bold">{{ tickets.mantenimientos }}</span> Próximos mantenimientos</p>
+  
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'SituacionTickets',
+    data() {
+      return {
+        tickets: {
+          activas: 50,
+          pendientes: 17,
+          sinAsignar: 23,
+          mantenimientos: 1
+        }
+      };
+    }
+  };
+  </script>
