a46470341a98f72b7e4e8d1152bcb0c69eef7201
Adrian Lopez <adriiloma2005@gmail.com>
2025-01-16 20:11:59 +0100

    quitado fondo componente

--- Codigo/uxify-frontend/src/components/CuadrosDatos.vue
+++ Codigo/uxify-frontend/src/components/CuadrosDatos.vue
@@ -1,5 +1,5 @@
 <template>
-    <Container class="bg-primary text-white py-2">
+    <Container class="text-white py-2">
       <div class="container-fluid">
         <div class="row align-items-center p-2" >
           <div class="col-md-12 d-flex justify-content-end pb-3" >
