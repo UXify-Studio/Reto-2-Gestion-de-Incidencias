d2e65b28a1838cef92427107420255d20b58414f
jasonEgibide <jasonalejandro.varas@ikasle.egibide.org>
2025-01-24 12:31:21 +0100

    paginacion Mantenimientos

--- Codigo/uxify-frontend/src/components/Header.vue
+++ Codigo/uxify-frontend/src/components/Header.vue
@@ -11,6 +11,14 @@
         </div>
         <div class="col-md-6 d-flex justify-content-end">
           <div class="d-flex align-items-center">
+            <i class="bi bi-bell me-3">
+              <img src="../assets/Notifi.png" class="icon" alt="Notification">
+            </i>
+            <router-link to="/register" class="text-white me-3">
+              <i class="bi bi-gear">
+                <img src="../assets/Config.png" class="icon" alt="Settings">
+              </i>
+            </router-link>
             <a @click.prevent="logout" class="text-white" href="#">
               <i class="bi bi-power">
                 <img src="../assets/Session.png" class="icon" alt="Logout">
